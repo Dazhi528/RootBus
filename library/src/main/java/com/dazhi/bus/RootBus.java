a0dd1ab8e5ceb8874d360d5a2c22724d8102405b
dazhi <1041665128@qq.com>
2020-12-08 17:36:21 +0800

    a

--- library/src/main/java/com/dazhi/bus/RootBus.java
+++ library/src/main/java/com/dazhi/bus/RootBus.java
@@ -1,10 +1,6 @@
 package com.dazhi.bus;
 
-import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +13,7 @@ import java.util.Map;
  * 修改日期：2018/4/19 14:55
  */
 public class RootBus {
-    private final Map<String, ObservableImpl<Object>> mapBusEvent;  // 存放LiveEvent
+    private final Map<String, ObservableImpl<Object>> mapBusEvent;
 
     private RootBus() {
         mapBusEvent = new HashMap<>();
@@ -27,51 +23,23 @@ public class RootBus {
         static final RootBus INSTANCE = new RootBus();
     }
 
-    public static RootBus self() {
+    private static RootBus self() {
         return classHolder.INSTANCE;
     }
 
-
-
-
-
-    private Observable<Object> get(@NonNull String key) {
-        return get(key, Object.class);
+    public static Observable<Object> get(@NonNull String key) {
+        return self().get(key, Object.class);
     }
-    private <T extends Observable> Observable<T> get(@NonNull Class<T> eventType) {
-        return get(eventType.getName(), eventType);
+
+    public static <T extends Observable> Observable<T> get(@NonNull Class<T> eventType) {
+        return self().get(eventType.getName(), eventType);
     }
+
     private synchronized <T> Observable<T> get(@NonNull String key, @NonNull Class<T> type) {
         if (!mapBusEvent.containsKey(key)) {
             mapBusEvent.put(key, new ObservableImpl<>());
         }
         return (Observable<T>) mapBusEvent.get(key);
-    }
-
-
-
-    public void post(Object event) {
-
-    }
-
-    @MainThread
-    public void register(@NonNull LifecycleOwner owner, @NonNull Observer observer) {
-
-    }
-
-    @MainThread
-    public void registerForever(@NonNull Observer observer) {
-
-    }
-
-    @MainThread
-    public void unregister(@NonNull Observer observer) {
-
-    }
-
-    @MainThread
-    public void unregisters(@NonNull LifecycleOwner owner) {
-
     }
 
 }
